44f19454c985ba8ef787c86def9237c8da057059
fardin-hasan <19-40043-1@student.aiub.edu>
2020-10-19 13:34:41 -0400

    lab1 and lab2

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        /*{
+
+            Console.WriteLine("Hello");
+            Console.WriteLine("Fardin Hasan Swadhin");
+        }
+    */
+
+        /*{
+            int a = 2;
+            int b = 3;
+            int sum;
+            sum = a + b;
+            Console.WriteLine("the sum is "+sum);
+         
+
+         }
+        */
+        /*{
+            int a = 36;
+            int b = 3;
+            int div;
+            div=a / b;
+            Console.WriteLine("the result is " +div);
+
+        }*/
+        /*{
+            int a = 5;
+            int b = 8;
+            int c = 6;
+            int result;
+            result = -a + b * c;
+            Console.WriteLine("Result is " + result);
+           Console.WriteLine((55+9)%9);
+            Console.WriteLine(20+(-3*5)/5);
+            Console.WriteLine(5+15/3*2-8%3);
+
+        }*/
+        /*{
+            int a=6;
+            int b=2;
+            
+            int add = a + b;
+            int sub = a - b;
+            int mul = a * b;
+            int div = a / b;
+            int rem = a % b;
+            Console.WriteLine("add "+add);
+            Console.WriteLine("sub " + sub);
+            Console.WriteLine("mul " + mul);
+            Console.WriteLine("mul " + mul);
+            Console.WriteLine("rem " + rem);
+        }
+        */
+        /*{
+            Console.WriteLine((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+        }*/
+        /*{
+            double width = 5.5;
+            double height = 8.5;
+            double area = width * height;
+            double perimeter = 2 * width + height;
+            Console.WriteLine("area " + area);
+            Console.WriteLine("perimeter " + perimeter);
+        }*/
+  
+        
+    }
+}
---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*{ int i;
+                int n = 100;
+
+                for (i = 1; i <= n; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine("total " + i);
+                    }
+                }
+            }
+            */
+
+            /*
+            {
+                int i;
+                int num=100;
+                int oddSum = 0;
+                int evenSum=0;
+                
+                for(i=1;i<=num;i++)
+                {
+                    if (i % 2 == 0)
+                        evenSum = evenSum + i;
+                    else
+                        oddSum = oddSum + i;
+                }
+                Console.WriteLine("sum of odd " + oddSum);
+                Console.WriteLine("sum of even" + evenSum);
+
+            }
+            */
+
+
+
+
+
+            /* for (int i=0;i<=10;i++)
+             {
+                 int x = i % 2;
+                 if (x == 0)
+                 {
+                     Console.Write(i + " ");
+                 }
+             }*/
+
+            //task2
+            /* int x = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j <= 5; j++)
+                {
+                    x = i + j;
+                    if (x%2==0)
+                    {
+                        Console.Write("x");
+                    }
+                    else
+                    {
+                        Console.Write("0");
+                    }
+
+                   
+                }
+                Console.WriteLine();
+            }*/
+            //task3
+            /*int x = 0;
+            for (int i = 1; i <=6; i++)
+            {
+                x = i;
+                for (int j = 0; j <= 3; j++)
+                {
+
+                    
+                        Console.Write(x+j);
+
+                    
+
+                  
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }*/
+
+
+
+
+
+        }
+    }
+}
